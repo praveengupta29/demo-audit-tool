20651f18cff1d338ccbbc10cf72bbf9a0d73fc04
Praveen Gupta <pragupta19@publicisgroupe.net>
2020-08-09 23:46:59 +0530

    Update config

--- microservices/questionnaire/config/configuration.ts
+++ microservices/questionnaire/config/configuration.ts
@@ -2,8 +2,6 @@ export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   secretKey: process.env.SECRET_KEY || 'my-secret-key',
   database: {
-    uri:
-      process.env.DB_URI ||
-      'mongodb+srv://pragupta19:test@123@gql-doyxq.mongodb.net/audit-tool?retryWrites=true&w=majority',
+    uri: process.env.DB_URI || 'mongodb://localhost:27017/audit-tool',
   },
 });
